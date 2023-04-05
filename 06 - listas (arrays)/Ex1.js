8908cb994ecd4a4d248300a6990e5f283643ea4b
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-04 23:50:49 -0300

    Aula 06 - Praticando Estruturas de Repetição

---
+++ 06 - listas (arrays)/Ex1.js
@@ -0,0 +1,6 @@
+//1 ) Crie um programada que dado um numero imprima a sua tabuada.
+const tabuada = 5;
+
+for (let i = 0; i <= 10; i++) {
+
+}
---
+++ 06 - listas (arrays)/Ex2.js
@@ -0,0 +1,13 @@
+//2) Crie um programa que seja capas de percorer uma lista de numeros e imprima numero par encontrado.
+
+
+const numeros = [0, 1, 2, 3 ,4 ,5 ,6 ,7 ,8, 9, 10];
+
+
+for (let i = 0; i < numeros.length; i++) {
+    const numero = numeros[i]; 
+    if(numero % 2 === 0) {
+        console.log(numero);
+    }
+    
+}
