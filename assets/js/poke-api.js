8775bea16a6440104722846b98915efc68ee44fe
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-10 22:48:52 -0300

    Revisao de codigo e entendendo os proximos passos

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -5,5 +5,13 @@
         return fetch(url)
             .then((response) => response.json())
             .then((jsonBody) => jsonBody.results)
-            .catch((error) => console.error(error))
     }
+
+Promise.all([
+    fetch('https://pokeapi.co/api/v2/pokemon/1'),
+    fetch('https://pokeapi.co/api/v2/pokemon/2'),
+    fetch('https://pokeapi.co/api/v2/pokemon/3'),
+    fetch('https://pokeapi.co/api/v2/pokemon/4')
+]).then((results) =>{
+    console.log(results)
+})
