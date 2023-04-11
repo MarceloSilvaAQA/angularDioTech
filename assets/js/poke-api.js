26f93734a2b70743f37164295282150c956d4f52
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-10 23:46:21 -0300

    Convertendo modelo Poke API

--- assets/js/poke-api.js
+++ assets/js/poke-api.js
@@ -1,7 +1,25 @@
     const pokeApi = {}
 
+    function convertPokeApiDetailToPokemon (pokemonDetail) {
+        const pokemon = new Pokemon()
+        pokemon.number = pokemonDetail.order
+        pokemon.name = pokemonDetail.name
+
+        const types = pokemonDetail.types.map((typeSlot) => typeSlot.type.name)
+        const [type] = types
+
+        pokemon.types = types
+        pokemon.type = type
+
+        pokemon.photo = pokemonDetail.sprites.other.dream_world.front_default
+
+        return pokemon
+
+    }
     pokeApi.getPokemonsDetail = (pokemon) => {
-       return fetch(pokemon.url).then((response) => response.json())
+       return fetch(pokemon.url)
+       .then((response) => response.json())
+       .then(convertPokeApiDetailToPokemon)
     }
 
     pokeApi.getPokemons = (offset = 0, limit = 5) => {
