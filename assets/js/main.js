f62f9990784f48280c4720348990ef3033acc9f3
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-10 22:17:39 -0300

    Separando consumo da api da manipulação de HTML

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,4 @@
 
-const offset = 0
-const limit = 10
-const url = `https://pokeapi.co/api/v2/pokemon?toffset=${offset}&limit=${limit}`
-
 function convertPokemonToLi(pokemon) {
     return `
     <li class="pokemon">
@@ -21,17 +17,17 @@ function convertPokemonToLi(pokemon) {
         </li>
     `
 }
+
+
 const pokemonList = document.getElementById('pokemonList')
 
-
-fetch(url)
-.then((response) => response.json())
-.then((jsonBody) => jsonBody.results)
-.then((pokemons) => {
+pokeApi.getPokemons().then((pokemons) => {
+    const listItems = []
     for (let i = 0; i < pokemons.length; i++) {
         const pokemon = pokemons[i];
-       pokemonList.innerHTML += convertPokemonToLi(pokemon)
+        listItems.push(convertPokemonToLi(pokemon))
     }
+    console.log(listItems)
 })
-.catch((error) => console.error(error))
+
 
