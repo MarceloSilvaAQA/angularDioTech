c4cc834c2e64f2297a6ce11ac017a80c31c5b5d0
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-10 21:30:54 -0300

    Manipulação de resultado da requisição atraves da promise

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,8 @@ const offset = 0
 const limit = 10
 const url = `https://pokeapi.co/api/v2/pokemon?toffset=${offset}&limit=${limit}`
 
-fetch(url).then(function (response) {
-    console.log(response)
-})
+fetch(url)
+.then((response) => response.json())
+.then((jsonBody) => console.log(jsonBody))
+.catch((error) => console.error(error))
+
