5c550d8d44f9f9c07cb933686b1d139c50121959
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-25 22:24:48 -0300

    Criando Components - Setando Propriedade de classes

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -3,14 +3,26 @@ class Cardnews extends HTMLElement{
         super();
         
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>hello</h1>";
-    }
+        shadow.appendChild(this.build());
+        shadow.appendChild(this.styles());
 
     build(){
+        const componentRoot = document.createElement("div");
+        componentRoot.setAttribute("class", "card");
 
+        const cardLeft = document.createElement("div");
+        cardLeft.setAttribute("class", "card__left")
+        const cardRight = document.createElement("div");
+        cardRight.setAttribute("class", "card__right");
+
+        componentRoot.appendChild(cardLeft);
+        componentRoot.appendChild(cardRight);
+
+        return componentRoot;
     }
 
-    styles(){}
+        styles(){}
+    }
 }
 
 customElements.define("card-news", Cardnews);
