9d7cad06708c74bd955b11f2ae9b51a51d1ba572
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-25 22:14:06 -0300

    Criando components - Construindo Component de Maneira Profissional

--- src/Components/CardNews.js
+++ src/Components/CardNews.js
@@ -1,10 +1,16 @@
- class CardNews extends HTMLElement {
+class Cardnews extends HTMLElement{
     constructor(){
         super();
         
         const shadow = this.attachShadow({mode: "open"});
-        shadow.innerHTML = "<h1>Hello World</h1>";
+        shadow.innerHTML = "<h1>hello</h1>";
     }
- }
 
- customElements.define('card-news', CardNews);
+    build(){
+
+    }
+
+    styles(){}
+}
+
+customElements.define("card-news", Cardnews);
