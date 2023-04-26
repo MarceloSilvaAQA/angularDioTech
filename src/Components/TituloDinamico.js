39fbea9d4a08444c8bd04ecb65a85f2bf7497ec9
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-04-25 21:50:13 -0300

    Criando Components - Estilizando um component

---
+++ src/Components/TituloDinamico.js
@@ -0,0 +1,25 @@
+class TituloDinamico extends HTMLElement{
+    constructor(){
+        super();
+
+        const shadow = this.attachShadow({mode: "open"});
+
+
+        //Base do component <h1> Marcelo</h1>
+        const componentRoot = document.createElement("h1");
+        componentRoot.textContent = 'Marcelo'
+
+        //estilizar o component
+         const style = document.createElement("style");
+         style.textContent = `
+         h1{
+            color: red;
+         }
+         `;
+        //enviar para a shadow
+        shadow.appendChild(componentRoot);
+        shadow.appendChild(style);
+    }
+}
+
+customElements.define('titulo-dinamico', TituloDinamico)
