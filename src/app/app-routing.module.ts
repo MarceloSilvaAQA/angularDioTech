dbdcd0d131dda7695c375c2a5d7583f5ae2eec80
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-05-10 20:37:28 -0300

    Trabalhando com rotas Angular - Recuperando parametros das rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,7 +5,7 @@ import { CardComponent } from './pages/portifolio/card/card.component';
 
 const routes: Routes = [
   {path: '', component: TitleComponent, pathMatch: 'full'},
-  {path: 'portifolio', component: CardComponent},
+  {path: 'portifolio/:id', component: CardComponent},
   {path: '**', redirectTo: ''}
 ];
 
--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-card',
@@ -7,7 +8,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CardComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private parametrizador: ActivatedRoute) {
+      this.parametrizador.params.subscribe(
+        res => console.log(res)
+      )
+     }
 
   ngOnInit(): void {
   }
