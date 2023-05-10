f4c3544043c70ac43040572febe3298715f816ba
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-05-10 20:24:42 -0300

    Trabalhando com rotas Angular - Navengação entre paginas com ROuterLink

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,10 +6,12 @@ import { AppComponent } from './app.component';
 
 import { IndexModule } from './pages/index/index.module';
 import { PortifolioModule } from './pages/portifolio/portifolio.module';
+import { MenuComponent } from './shared/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
