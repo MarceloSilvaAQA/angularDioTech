c0e93ca86cfb09e6db544f1d090c8c9f9dfc6b6e
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-05-11 19:30:54 -0300

    Trabalhando com Services Angular - Preparando estrutura de Pastas

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,7 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
+  template: `
+  <h1>Aula de Services</h1>
+  <app-card></app-card>
+  <router-outlet></router-outlet>
+
+  `,
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
