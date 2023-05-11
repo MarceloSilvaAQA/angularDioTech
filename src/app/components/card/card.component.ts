34b82c49a39c0ec2ffc70d9dad9e66644903ccd8
Marcelo Henrique da Silva <marcelo@pop-os.localdomain>
2023-05-11 20:20:19 -0300

    Trabalhando com Services Angular - Como Injetar um Service

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PokemonService } from 'src/app/services/pokemon.service';
 
 @Component({
   selector: 'app-card',
@@ -8,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 export class CardComponent implements OnInit {
   name: string = "BULBASSAUR";
   attributesTypes: string[] = ['FIRE', 'ROCK']
-  constructor() { }
+  constructor(
+    private service: PokemonService
+  ) {
+
+   }
 
   ngOnInit(): void {
+    this.service.getPokemon("Bulbassaur")
   }
 
 }
